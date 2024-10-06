5243e874ec27339818d9952fa72b00517ede3cb6
iiiCircinus <iii.circinus.com@gmail.com>
2024-10-06 16:05:57 +0700

    06/10/2024

--- template/XYZStarJob/XYZStar/XYZStar/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', include('Home.urls')),
-    # path('product/', include('Product.urls')),
-    path('contact/', include("Contact.urls")),
-]
